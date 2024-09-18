da5cf3ec64fe46d3180bff76ed91f016e601c7e6
ascarbek <ascarbek@gmail.com>
2024-09-18 13:53:33 +0500

    socket path env

--- modules/backend/src/server/Environment.ts
+++ modules/backend/src/server/Environment.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'someverysecretkey';
-
+export const SOCKET_PATH = process.env.SOCKET_PATH || '';
 export const SERVER_PORT: number = parseInt(process.env.PORT || '') || 8080;
 export const MYSQL_HOST: string = process.env.MYSQL_HOST || '';
 export const MYSQL_PORT: number = parseInt(process.env.MYSQL_PORT || '');
--- modules/backend/src/server/server.ts
+++ modules/backend/src/server/server.ts
@@ -2,7 +2,7 @@ import { App } from 'uWebSockets.js';
 import { OpenConnection } from './socket/OpenConnection.js';
 import { CloseConnection } from './socket/CloseConnection.js';
 import { ReceiveMessage } from './socket/ReceiveMessage.js';
-import { SERVER_PORT } from './Environment.js';
+import { SERVER_PORT, SOCKET_PATH } from './Environment.js';
 import { LogMessage } from './services/Logger.js';
 
 export const startServer = () => {
@@ -10,7 +10,7 @@ export const startServer = () => {
   const server = App({});
 
   server
-    .ws<{ socket_id: string }>('/game', {
+    .ws<{ socket_id: string }>(SOCKET_PATH, {
       open: OpenConnection,
       close: CloseConnection,
       message: ReceiveMessage,
